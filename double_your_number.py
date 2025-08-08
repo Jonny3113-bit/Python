b4a3475d1951dcd4ae855107c196e32c0fdc801d
Mayur Singh <ms7702826@gmail.com>
2025-08-08 21:27:58 +0530

    practice

---
+++ double_your_number.py
@@ -0,0 +1,9 @@
+# fav_num = input("input your favriout number")
+# double_number = int(fav_num)
+# dob_fav_nu_nummber = fav_num * 2
+# print(f"your fav number is{dob_fav_number}")
+
+hobbie = ["dancing","running","studying"]
+
+for i in hobbie:
+    print(f"i enjoy{i}")
