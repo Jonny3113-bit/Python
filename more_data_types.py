61e787aeb932171759ad882ce5c10ff30badbeb6
Mayur Singh <ms7702826@gmail.com>
2025-08-08 21:08:22 +0530

    array manipulation

---
+++ more_data_types.py
@@ -0,0 +1,25 @@
+# arr =[]
+
+# arr.append(123)
+# arr.append(456)
+# arr.append("mayur")
+# print(arr)
+
+# marks = {}
+# print(type(marks))
+
+# marks["math"] = 90
+# marks["hindi"] = 50
+# marks["ss"]= 60.9
+
+# print(f"type of marks is {type(marks["math"])}")
+# print(f"type of marks is {type(marks["hindi"])}")
+
+# ////////////////////////////////
+
+sum = {}
+
+arr = [1,2,3]
+sum[arr]=6
+
+print(sum)
